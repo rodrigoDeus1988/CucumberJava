df50c7fc06de56edccf1444ae86884c39dd38d79
Rodrigo Florindo <rodrigo.florindo@yaman.com.br>
2018-07-17 15:17:19 -0300

    Classe Runner

--- CursoCucumber/src/test/java/Runner/Runner.java
+++ CursoCucumber/src/test/java/Runner/Runner.java
@@ -6,10 +6,11 @@ import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/InserirConta.feature", glue = {
+@CucumberOptions(tags = "@Login_Sucesso, @Login_acesso_negado",features = "src/test/resources/features/Login.feature", glue = {
 		"" }, monochrome = true, dryRun = false)
 
  public class Runner { 
 
 	
 }
+ 
