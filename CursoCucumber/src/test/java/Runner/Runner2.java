37ffc7ec2354dc69ce1dafa202669b2770800b58
Rodrigo de Deus <rodrigo-florindo@hotmail.com>
2018-08-03 11:11:35 -0300

    classe

--- CursoCucumber/src/test/java/Runner/Runner2.java
+++ CursoCucumber/src/test/java/Runner/Runner2.java
@@ -5,9 +5,15 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags = "@Login_Sucesso", features = "src/test/resources/features/Curso/Login.feature", glue = {
+@CucumberOptions(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},tags = "@Login_Sucesso", features = "src/test/resources/features/Curso/Login.feature", glue = {
 		"" }, monochrome = true, dryRun = false)
 
-public class Runner2 {
 
+public class Runner2 {
+ 
 }
+
+
+
+
+
